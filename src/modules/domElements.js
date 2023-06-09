d497a735bea457af375e062d096aff3a124eb3f0
leloufadel <leloufadel@gmail.com>
2023-06-10 00:09:36 +0300

    fix linters

--- src/modules/domElements.js
+++ src/modules/domElements.js
@@ -1,5 +1,5 @@
 export const addForm = document.querySelector('.add-task-form');
 export const taskList = document.querySelector('.task-list-group');
 export const deleteTask = document.querySelector('.delete-task-btn');
-export const editTaskg = document.querySelector('.edit-task-btn');
-export const clearDoneList = document.querySelector('.clear-list-task');
+export const editTask = document.querySelector('.edit-task-btn');
+export const clearList = document.querySelector('.clear-list-task');
